b5c1f1917418772e6cd4bc54d762381ba123f96f
wolftxt <wolfdavid49@gmail.com>
2025-07-20 23:34:36 +0200

    Initialized spring project

---
+++ src/main/java/verbs/VerbsApplication.java
@@ -0,0 +1,13 @@
+package verbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/verbs/VerbsApplicationTests.java
@@ -0,0 +1,13 @@
+package verbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
