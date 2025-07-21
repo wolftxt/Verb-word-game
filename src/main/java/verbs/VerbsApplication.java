118d59b63cfd010a1342a9bfade2aae252bace3b
wolftxt <wolfdavid49@gmail.com>
2025-07-21 23:52:55 +0200

    Removed test which caused project to be unbuildable

--- src/main/java/verbs/VerbsApplication.java
+++ src/main/java/verbs/VerbsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VerbsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VerbsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VerbsApplication.class, args);
+    }
 
 }
